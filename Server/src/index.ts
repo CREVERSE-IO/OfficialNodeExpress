3073304688514f3d513989369f8fe88a760e30fb
ROBB <nggon@bnfgames.com>
2022-03-21 21:14:13 +0900

    Modify Image

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import process from "process"
 import bodyParser from "body-parser";
 import path from "path";
+import {createTransport, Transporter } from "nodemailer";
 
 const port: number = 81;
 
 class App {
     public application: express.Application;
-
     constructor() {
         this.application = express();
     }
@@ -24,9 +23,77 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use( "/", express.static( path.join( __dirname, "static" ) ) );
-app.get( "/", (req, res)=>{
-    res.send({
-        message:"Hello"
-    });
+app.get( "/hello", (req, res)=>{
+    console.log("Hello");
 });
+
+app.post("/reqMailer", (req, res) => {
+    let validate: boolean = true;
+    let errorString: string = "";
+    let result: string = "Success"
+
+    if ( req == null ) {
+        res.send({type: "error", text: "Message is Null"});
+    } else {
+        if ( req.body.userName.length < 4 ) {
+            validate = false;
+            errorString = "Name is too short or empty!";
+            result = "error";
+        }
+
+        let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+        if ( req.body.userEmail.match(regExp) == null ) {
+            validate = false;
+            errorString = "Please enter a valid email!";
+            result = "error";
+        }
+
+        if ( req.body.userMessage.length < 5 ) {
+            validate = false;
+            errorString = "Too short message! Please enter something.";
+            result = "error";
+        }
+
+        if ( validate == true ) {
+            errorString = "Hi " + req.body.userName + "! Thank you for your email";
+            SendMail( req.body );
+        }
+    }
+    res.send({type: result, text: errorString });
+});
+
+async function SendMail( content: any ) {
+    let transporter = createTransport( {
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "creverse.contactus@gmail.com",
+            pass: "creverse!@34"
+        }
+    });
+
+    if ( null == transporter ) {
+        return;
+    }
+
+    let description: string = "Name: " + content.userName + "<br>" +
+        "E-Mail: " + content.userEmail + "<br>" +
+        "Message: " + content.userMessage + "<br>";
+
+    let info: any = null;
+    try {
+        info = await transporter.sendMail( {
+            from: "creverse.contactus@gmail.com",
+            to: "creverse.contactus@gmail.com",
+            subject: "Contact Message from Official Web Site",
+            text: "",
+            html: description
+        });
+    } catch (e) {
+
+        return;
+    }
+}
+
 const server = app.listen(port, () => { console.log("server initialized"); });
--- Server/src/static/js/contract-mailer.js
+++ Server/src/static/js/contract-mailer.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         var user_name = $('input[name=name]').val();
         var user_email = $('input[name=email]').val();
         var user_message = $('textarea[name=message]').val();
-        
+
         var proceed = true;
         if (user_name == "") {
             $('input[name=name]').css('border-color', '#e41919');
@@ -17,33 +17,29 @@ $(document).ready(function(){
             $('input[name=email]').css('border-color', '#e41919');
             proceed = false;
         }
-        
+
         if (user_message == "") {
             $('textarea[name=message]').css('border-color', '#e41919');
             proceed = false;
         }
-        
-        //everything looks good! proceed...
+
         if (proceed) {
-            var data = {
+            var profiles = {
                 'userName': user_name,
                 'userEmail': user_email,
                 'userMessage': user_message
             };
 
             $.post({
-                traditional: true,
-                url: '/reqMailer',
+                traditional: true, url: '/reqMailer',
                 contentType: 'application/json',
-                data: JSON.stringify( data ),
-                dataType: 'json',
-                success: function ( response ) {
+                data: JSON.stringify( profiles ), dataType: 'json',
+                success: function (response) {
                     if (response.type == 'error') {
                         output = '<div class="error">' + response.text + '</div>';
                     }
                     else {
                         output = '<div class="success">' + response.text + '</div>';
-
                         $('#contact_form input').val('');
                         $('#contact_form textarea').val('');
                     }
@@ -51,14 +47,31 @@ $(document).ready(function(){
                     $("#result").hide().html(output).slideDown();
                 }
             });
+
+
+
+            // $.post('contact_me.php', post_data, function(response){
+            //     if (response.type == 'error') {
+            //         output = '<div class="error">' + response.text + '</div>';
+            //     }
+            //     else {
+            //         output = '<div class="success">' + response.text + '</div>';
+            //         $('#contact_form input').val('');
+            //         $('#contact_form textarea').val('');
+            //     }
+            //
+            //     $("#result").hide().html(output).slideDown();
+            // }, 'json');
+
         }
+
         return false;
     });
-    
+
     //reset previously set border colors and hide all message on .keyup()
     $("#contact_form input, #contact_form textarea").keyup(function(){
         $("#contact_form input, #contact_form textarea").css('border-color', '');
         $("#result").slideUp();
     });
-    
+
 });
