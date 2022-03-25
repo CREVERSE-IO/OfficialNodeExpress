b4fb9d7539292aac4341d6cd4a0ba2153e3ab2df
ROBB <nggon@bnfgames.com>
2022-03-25 18:11:14 +0900

    revert pdf

--- Server/src/static/js/pdfviewer.js
+++ Server/src/static/js/pdfviewer.js
@@ -14,35 +14,39 @@ const viewerConfig = {
     embedMode: "IN_LINE"
 };
 
-if (window.AdobeDC) {
-    previewFile({
-        content: {
-            location: {
-                url: "http://cre-verse.io/pdf/whitepaper.pdf",
-            },
-        },
-        metaData: {
-            fileName: "whitepaper.pdf"
-        }
-    }, viewerConfig);
-} else {
+/* Wait for Adobe Document Services PDF Embed API to be ready */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    console.log("adobe_dc_view_sdk.ready");
+    /* Initialize the AdobeDC View object */
     var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
         clientId: "f1065ee686c048229eaef8a9eaa1e3c0",
+        /* Pass the div id in which PDF should be rendered */
         divId: "adobe-dc-view",
     });
 
-    document.addEventListener("adobe_dc_view_sdk.ready", () => {
-        previewFile({
-            content: {
-                location: {
-                    url: "http://cre-verse.io/pdf/whitepaper.pdf",
-                },
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "http://cre-verse.io/pdf/whitepaper.pdf",
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
             },
-            metaData: {
-                fileName: "whitepaper.pdf"
-            }
-        }, viewerConfi);
-    });
-}
-
-
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "whitepaper.pdf"
+        }
+    }, viewerConfig);
+});
